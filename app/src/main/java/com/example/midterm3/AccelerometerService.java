126405a356b1c9f4ed33b449d1a30ddbc3b80af1
Naoki_MacBook <naokigsyoshida@gmail.com>
2023-05-29 07:07:47 +0900

    注意喚起が連続的にならないよう修正

--- app/src/main/java/com/example/midterm3/AccelerometerService.java
+++ app/src/main/java/com/example/midterm3/AccelerometerService.java
@@ -4,16 +4,25 @@ package com.example.midterm3;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.IBinder;
+import android.os.SystemClock;
 import android.util.Log;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 public class AccelerometerService extends Service implements SensorEventListener {
     private SensorManager sensorManager;
     private static final String TAG = "AccService";
+
+    private long lastStartActivityTime = -1;
+    private static long marginTime = 5000; // 再度警告までの猶予
 
     @Override
     public void onCreate() {
@@ -46,18 +55,56 @@ public class AccelerometerService extends Service implements SensorEventListener
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
             // z軸の加速度の値を取得
             float zValue = event.values[2];
+            float yValue = event.values[1];
+
 
             // z軸の加速度が-9を下回った場合、アクティビティを起動
             if (zValue < -9) {
-                Intent intent = new Intent(this, MyActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
+                long time_now = SystemClock.elapsedRealtime();
+//                Log.d(TAG, String.format("time now = %d", time_now));
+//                Log.d(TAG, String.format("last now = %d", lastStartActivityTime));
+                if (lastStartActivityTime+marginTime<time_now){
+                    notification();
+                    lastStartActivityTime = time_now;
+                }
             }
         }
+    }
+
+    public void notification() {
+        Intent intent = new Intent(this, MyActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        // 通知の実行
+        showNotification();
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    private void showNotification() {
+        // 通知を作成
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "channel_id")
+                .setSmallIcon(R.drawable.smartphone_neru_boy_smash)
+                .setContentTitle("通知タイトル")
+                .setContentText("通知メッセージ")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        // 通知を表示
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Log.d(TAG, "notification_failed");
+            return;
+        }
+        notificationManager.notify(1, builder.build());
     }
 }
